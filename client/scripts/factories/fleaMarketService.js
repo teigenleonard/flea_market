ff42d2adb8e1dbb715f8a54337c09d30179302a6
Chris Keller <cjameskeller@gmail.com>
2017-04-19 17:51:36 -0500

    balance functions

--- client/scripts/factories/fleaMarketService.js
+++ client/scripts/factories/fleaMarketService.js
@@ -4,8 +4,19 @@ myApp.factory('fleaMarketService', function() {
 //class MarketItem {currentPrice, name, quanity, avePrice(), iniital price -> stretch}
   //children (smallElec, fruits, collectables)
 
-//updateBalance() -->add/subtract --> return accountBalance  
+//updateBalance() -->add/subtract --> return accountBalance
 
+let myBalance = {
+  amount: 100
+};
+
+let buyFunc = (thisItem) => {
+  myBalance = myBalance - thisItem.currentPrice;
+};
+
+let sellFunc = (thisItem) => {
+  myBalance = myBalance - thisItem.currentPrice;
+};
 
   return {
 
