b8f409c69f2351acaf09497c9b004404f5c07893
Bri Dickman <brianna.dickman@gmail.com>
2017-04-20 09:21:31 -0500

    adds in var myApp

--- client/scripts/controllers/buy.js
+++ client/scripts/controllers/buy.js
@@ -5,22 +5,5 @@
 myApp.controller('BuyController', ['fleaMarketService', function(fleaMarketService) {
   console.log("BuyController sourced!");
 
-  // var buy = this;
-  // console.log(this);
-  // buy.currentPrice = setInterval(adjPrice(currentPrice), 15000);
-  //
-  // var currentPrice = 1.35;
-  //
-  // var setTimer = setInterval(adjPrice(currentPrice), 15000);
-  //
-  // function adjPrice(currentPrice){
-  //   var max = 0.5;
-  //   var min = -0.5;
-  //   currentPrice = Math.floor(Math.random() * (max - min + 1)) + min;
-  //   console.log(currentPrice);
-  //   return currentPrice;
-  // }
-  //
-  // adjPrice(currentPrice);
 
 }]);
